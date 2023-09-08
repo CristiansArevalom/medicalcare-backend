ec18c2f971c53a880ef5da320f8c49fb65e142a9
Cristian Arevalo <82003174+CristiansArevalom@users.noreply.github.com>
2023-09-08 17:37:44 -0500

    preparing job enviroment and initial springboot configuration

---
+++ src/test/java/com/medicalcare/medicalcarebackend/MedicalcareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicalcare.medicalcarebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalcareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
